b617ee790319ac8b94e677ea57ad15df977ec42e
Arthur Rousseau <arthur.rousseau@bts-malraux.net>
2015-06-01 22:58:44 +0200

    Visibility states changes using FontAwesome, TotemCtrl debug

--- public/app/TotemCtrl.js
+++ public/app/TotemCtrl.js
@@ -1,11 +1,17 @@
 'use strict';
 angular.module('FileSync').controller('TotemCtrl', ['SocketIOService',
   function (SocketIOService) {
-    this.isAdmin = SocketIOService.amIAdmin(function(amIAdmin){
-    	amIAdmin;
-	});
+    var TotemControl = this;
 
-    var isAdmin = 'blabla';
+    // debug 001
+    TotemControl.isAdmin = 'Value before';
+
+    this.isAdmin = SocketIOService.amIAdmin(function(amIAdmin){
+    	return amIAdmin;
+    });
+
+    // debug 002
+    this.isAdmin = 'Value after';
     this.changeTotemOwner = function() {
       SocketIOService.changeTotemOwner();
     };
