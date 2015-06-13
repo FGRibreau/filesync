27aac903bc7e2043964809fc415c06c6248080e0
FG Ribreau <github@fgribreau.com>
2015-06-13 19:38:10 +0200

    feat(deps): upgrade winston, common-env and async

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function (logger) {
-  var env = require('common-env')(logger);
+  var env = require('common-env/withLogger')(logger);
 
   return env.getOrElseAll({
     auth: {
